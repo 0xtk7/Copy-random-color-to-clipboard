54ddcf7d5610c1134a4a8c6083466314f19a8045
0xtk7 <harrisonelvin01@gmail.com>
2022-01-29 16:32:36 +0000

    Added src directory
    Organise code

---
+++ src/script.js
@@ -0,0 +1,17 @@
+var root = document.documentElement;
+
+// Random background color
+function randomColor() {
+    var color = Math.floor(Math.random()*16777215).toString(16);
+    color = '#' + color;
+    root.style.setProperty('--BackgroundColor', color);
+}
+
+// copy background color
+function getColor() {
+    var content = root.style.getPropertyValue('--BackgroundColor').toUpperCase();
+    navigator.clipboard.writeText(content);
+    alert('Copied '+content+' to clipboard!');
+}
+
+
