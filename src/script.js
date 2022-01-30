9370a411c87eb19b3c4c437c1d54805a00d3c1eb
0xtk <86887490+0xtk7@users.noreply.github.com>
2022-01-30 14:36:01 +0000

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,15 @@
+var root = document.documentElement;
+
+// Random background color
+function randomColor() {
+    var color = Math.floor(Math.random()*16777215).toString(16);
+    color = '#' + color;
+    root.style.setProperty('--BackgroundColor', color);
+}
+
+// Copy background color (Web)
+function getColor() {
+    var content = root.style.getPropertyValue('--BackgroundColor').toUpperCase();
+    clipboard.writeText(content);
+    alert('Copied '+content+' to clipboard!');
+}
